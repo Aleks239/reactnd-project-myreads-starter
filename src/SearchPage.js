9118bb524db8ffb99dac96615f6ffd6c0b510efe
Aleksandr Romanov <alexromanov239@gmail.com>
2017-08-20 20:03:49 +0300

    type: feat
    Add search books functionality via SearchPage component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 import * as BooksAPI from './BooksAPI'
 import {Link} from "react-router-dom"
+import Book from "./Book"
 
-function SearchPage(props) {
+class SearchPage extends Component {
 
-    return(
-        <div className="search-books">
-            <div className="search-books-bar">
-                <Link to="/" className="close-search">Close</Link>
-                <div className="search-books-input-wrapper">
-                    {/*
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: "",
+            books: [],
+            loading: false
+
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({query: e.target.value})
+    }
+
+
+    enter = (e) => {
+        let query = e.target.value
+
+        if (e.key === "Enter" && query !== "") {
+            this.setState({loading: true})
+            console.log(query.trim())
+            BooksAPI.search(query, 10).then((data) => {
+                if (Array.isArray(data)) {
+                    console.log(data)
+                    this.setState({
+                        books: data,
+                        loading: false
+                    })
+
+                }
+                else {
+                    this.setState({
+                        loading: false,
+                        books: []
+                    })
+                }
+            })
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div onKeyPress={this.enter} className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -17,15 +61,30 @@ function SearchPage(props) {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                    <input type="text" placeholder="Search by title or author"/>
+
+
+                        <input value={this.state.query} onChange={this.handleChange} type="text"
+                               placeholder="Search by title or author"/>
+
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+
+                    {this.state.loading ? <h1>Loading...</h1> :
+                        <ol className="books-grid">{this.state.books.map((book) => {
+                            return <Book type={book.shelf} makeChange={this.changeShelf} bookRef={book}
+                                         key={book.id} author="Shit" title={book.title}
+                                         cover={book.imageLinks.thumbnail}/>
+                        })}</ol>}
+
 
                 </div>
             </div>
-            <div className="search-books-results">
-                <ol className="books-grid"></ol>
-            </div>
-        </div>
-    )
+        )
+
+    }
+
 
 }
 
